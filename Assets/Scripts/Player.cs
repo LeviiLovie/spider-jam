c693a37f559240b83acc630d4e1be85cf935c22a
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-01-07 11:26:00 +0700

    Create Moving Spider on Web

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,77 +7,76 @@ public class Player : MonoBehaviour
     [Header("Movement Settings")]
     public float MovementSpeed;
     public float JumpSpeed;
-    public GameObject SpawnPoint;
-    public KeyCode RespawnKeyCode;
+    public GameObject Point1;
+    public GameObject Point2;
 
-    [Header("Web Settings")]
-    public float WebMovingSpeed;
-    public float WebLenght;
-    public GameObject MousePositionObject;
-    public KeyCode WebUsingKeyCode;
-    public LayerMask WebLayers;
-
-    // private bool isWeb;
-    // private Vector2 webVector2;
+    private bool isWeb;
+    private Vector2 webVector2;
     private Vector2 movingVector2;
     private Vector2 mousePositionInUnits;
 
-    // void Start()
-    // {
-    //     if (this.GetComponent<Rigidbody2D>())
-    //     {
-    //         GetComponent<Rigidbody2D>().freezeRotation = true;
-    //     }
-    // }
+    private Rigidbody2D _rigitBody2d;
 
-    void FixedUpdate()
+    void Start()
     {
-        // if (!isWeb) {
-            movingVector2 = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") * JumpSpeed);
-            transform.Translate(movingVector2 * Time.deltaTime * MovementSpeed, Space.World);
-           
-            mousePositionInUnits = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            MousePositionObject.transform.position = mousePositionInUnits;
-        // }
-        // else {
-        //     Vector2 position = transform.position;
-        //     Debug.Log((webVector2 - position).magnitude + " - " + WebMovingSpeed);
-        //     if ((webVector2 - position).magnitude < WebMovingSpeed) {
-        //         transform.position = webVector2;
-        //         isWeb = false;
-        //     } else {
-        //         Vector2 webVector2Normalized = webVector2;
-        //         webVector2Normalized.Normalize();
-        //         Debug.Log(webVector2Normalized + " - " + webVector2);
-        //         transform.Translate(webVector2Normalized * WebMovingSpeed);
-        //     }
-        // }
+        if (this.GetComponent<Rigidbody2D>())
+        {
+            _rigitBody2d = GetComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(WebUsingKeyCode))
-        {
-            // Debug.Log("F");
-            Vector2 startPosition = transform.position;
-            // RaycastHit2D hit = Physics2D.Raycast(startPosition + new Vector2(0, -1), mousePositionInUnits, 1000.0f);
-            RaycastHit2D hit = Physics2D.Raycast(startPosition, mousePositionInUnits - startPosition, 1000.0f, WebLayers);
-            if (hit.collider != null)
+        if (isWeb) {
+            Physics.gravity = new Vector3(0, 0, 0);
+
+            movingVector2 = (Point1.transform.position - Point2.transform.position);
+            movingVector2.Normalize();
+            Debug.Log(movingVector2);
+            if (Input.GetKey(KeyCode.A))
             {
-                Debug.Log(hit.point);
-                // Debug.DrawRay(startPosition, hit.point, Color.yellow);
-                // transform.position = hit.point - new Vector2(0, -1);
-                // isWeb = true;
-                Vector2 webVector2Normalized = hit.point - startPosition;
-                webVector2Normalized.Normalize();
-                this.GetComponent<Rigidbody2D>().AddForce(webVector2Normalized * WebMovingSpeed);
-                // transform.position = hit.point;
+                _rigitBody2d.AddForce(movingVector2);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                _rigitBody2d.AddForce(-movingVector2);
             }
         }
-        else if (Input.GetKeyDown(RespawnKeyCode))
+        else
         {
-            transform.position = SpawnPoint.transform.position;
-            // isWeb = false;
+            Physics.gravity = new Vector3(0, -1.0F, 0);
+        }
+    }
+
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(WebUsingKeyCode))
+    //     {
+    //         Vector2 startPosition = transform.position;
+    //         RaycastHit2D hit = Physics2D.Raycast(startPosition, mousePositionInUnits - startPosition, 1000.0f, WebLayers);
+    //         if (hit.collider != null)
+    //         {
+    //             Debug.Log(hit.point);
+    //             Vector2 webVector2Normalized = hit.point - startPosition;
+    //             webVector2Normalized.Normalize();
+    //             this.GetComponent<Rigidbody2D>().AddForce(webVector2Normalized * WebMovingSpeed);
+    //         }
+    //     }
+    // }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "web")
+        {
+            isWeb = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "web")
+        {
+            isWeb = false;
         }
     }
 }
