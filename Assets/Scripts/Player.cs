6f35dddc69a6649cb83ebe76b8f1f6c017d10cf7
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-01-07 22:44:42 +0700

    create not working player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,9 @@ public class Player : MonoBehaviour
     [Header("Movement Settings")]
     public float MovementSpeed;
     public float JumpSpeed;
-    public GameObject Point1;
-    public GameObject Point2;
 
     private bool isWeb;
+    private bool canWalk;
     private bool isJump;
     private Vector2 webVector2;
     private Vector2 movingVector2;
@@ -50,25 +49,29 @@ public class Player : MonoBehaviour
 
         //}
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            _rigitBody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MovementSpeed * Time.deltaTime, _rigitBody2d.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            _rigitBody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MovementSpeed * Time.deltaTime, _rigitBody2d.velocity.y);
-        }
-        else
-        {
-            _rigitBody2d.velocity = new Vector2(0, _rigitBody2d.velocity.y);
-        }
+        if (canWalk) {
+            if (Input.GetKey(KeyCode.A))
+            {
+                _rigitBody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MovementSpeed * Time.deltaTime, _rigitBody2d.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                _rigitBody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * MovementSpeed * Time.deltaTime, _rigitBody2d.velocity.y);
+            }
+            else
+            {
+                _rigitBody2d.velocity = new Vector2(0, _rigitBody2d.velocity.y);
+            }
 
-        if (Input.GetKey(KeyCode.Space) && !isJump)
-        {
-            _rigitBody2d.AddForce(this.transform.up * JumpSpeed, ForceMode2D.Impulse);
-            Debug.Log(this.transform.position + " pose");
-            Debug.Log(this.transform.up + " : up");
-            isJump = true;
+            if (Input.GetKey(KeyCode.Space) && !isJump)
+            {
+                _rigitBody2d.AddForce(this.transform.up * JumpSpeed, ForceMode2D.Impulse);
+                Debug.Log(this.transform.position + " pose");
+                Debug.Log(this.transform.up + " : up");
+                isJump = true;
+            }
+        } else {
+            // _rigitBody2d.velocity = new Vector2(0, -9.8f);
         }
     }
 
@@ -90,17 +93,21 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "web")
+        Debug.Log("enter");
+        if (other.gameObject.tag == "home")
         {
-            isWeb = true;
+            // isWeb = true;
+            canWalk = true;
+            Debug.Log("home");
         }
         isJump = false;
     }
     void OnCollisionExit2D(Collision2D other)
     {
-        if (other.gameObject.tag == "web")
+        if (other.gameObject.tag == "home")
         {
-            isWeb = false;
+            // isWeb = false;
+            canWalk = false;
         }
         isJump = true;
     }
